11a8f8fc29f22ef5c2e47687cedf02246dfc10d6
sakakendo <sakakendo0321@gmail.com>
2019-06-03 14:53:21 +0900

    add scheduler

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-
 import ardrone 
+from icecream import ic
+import signal
 drone=ardrone.ARDrone()
 
 # drone.takeoff()
 # drone.land()
 
-print(drone.navdata['demo']['battery'])
+sum = {
+    'theta',
+    'phi',
+    'psi',
+    'vx',
+    'vy',
+    'vz',
+}
 
-drone.image.show()
+def scheduler(arg1,arg2):
+    sum["theta"] += drone.navdata['demo']['theta']
+    sum["phi"] += drone.navdata['demo']['phi']
+    sum["psi"] += drone.navdata['demo']['psi']
+    sum["vx"] += drone.navdata['demo']['vx']
+    sum["vy"] += drone.navdata['demo']['vy']
+    sum["vz"] += drone.navdata['demo']['vz']
+    ic(sum)
 
-drone.halt()
+#drone.image.show()
+
+if __name__ == "__main__":
+    print("start program")
+    signal.signal(signal.SIGARALM,scheduler)
+    signal.settimer(signal.ITIMER_REAL,1,1)
+    drone.halt()
+    print("finished")
 
